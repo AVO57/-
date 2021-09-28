55580eab19c2f484fd6a9bed36c91ee0d1ae9abf
AVO57 <grigoryan.edmond16@mail.ru>
2021-09-28 09:49:42 +0500

    Добавьте файлы проекта.

---
+++ pz_5/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace pz_5
+// Задание №1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x;
+            for (x = 20; x <= 80; x += 5)
+            {
+                Console.Write(x + " ");
+            }
+        }
+    }
+}
+
+// Задание №2
+
+
