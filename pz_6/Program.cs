20ba18afedb281b80ca389d916d729556bab46dc
AVO57 <grigoryan.edmond16@mail.ru>
2021-09-29 13:09:42 +0500

    test edit

--- pz_6/Program.cs
+++ pz_6/Program.cs
@@ -6,18 +6,13 @@ namespace pz_6
     {
         static void Main(string[] args)
         {
-            int N = 0;
-            while (N <= 0)
-            {
-                N = Convert.ToInt32(Console.ReadLine());
-            }
-            int a = 3;
-       
-            if (N/a!=0)
-                Console.WriteLine ("True");
-            else
-                Console.WriteLine("False");
+            int N = int.Parse(Console.ReadLine());
 
+            while (N>1)
+            {
+                N /= 3;
+            }
+            Console.WriteLine(N == 1);
         }
     }
 }
