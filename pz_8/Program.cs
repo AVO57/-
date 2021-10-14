22c7624bb5c3ab64f8cece8cb9b52d2e1f4d5a5e
AVO57 <grigoryan.edmond16@mail.ru>
2021-10-14 11:34:08 +0500

    test edit

--- pz_8/Program.cs
+++ pz_8/Program.cs
@@ -6,7 +6,31 @@ namespace pz_8
     {
         static void Main(string[] args)
         {
-          int 
+            Random rnd = new Random();
+            int[] a = new int[10];
+            int[] b = new int[10];
+            Console.Write("Исходный массив:");
+            for (int i = 0; i < 10; i++) //Заполняем массив случайными данными
+            {
+                a[i] = rnd.Next(-10, 11);
+                Console.Write(" " + a[i]);
+            }
+            Console.WriteLine();
+            Console.Write("Массив после изменений:");
+            for (int i = 0; i < 5; i++)
+            {
+                b[i] = a[4 - i];
+            }
+            for (int i = 0; i < 5; i++)
+            {
+                b[i + 5] = a[9 - i];
+
+            }
+            for (int i = 0; i < 10; i++)
+            {
+                a[i] = b[i];
+                Console.Write(" " + a[i]);
+            }
         }
     }
 }
