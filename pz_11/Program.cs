bad180f6422e618d39d11ee9652c6feff18ed37d
AVO57 <grigoryan.edmond16@mail.ru>
2021-11-10 17:58:36 +0500

    test edit

---
+++ pz_11/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace pz_11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите текст:");
+            string InputString;
+            InputString = Console.ReadLine();
+            Console.WriteLine(String.Join(" ", InputString.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)));
+        }
+    }
+}
