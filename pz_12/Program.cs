dd6967a299f91b644c41356f853f29654408f37f
AVO57 <grigoryan.edmond16@mail.ru>
2021-11-11 17:12:57 +0500

    test edit

---
+++ pz_12/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace pz_12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ввдите текста: \t");
+
+            string str = Console.ReadLine();
+            string strrev = "";
+
+            foreach (var word in str.Split(' '))
+            {
+                string temp = "";
+                foreach (var ch in word.ToCharArray())
+                {
+                    temp = ch + temp;
+                }
+                strrev = strrev + temp + "";
+            }
+            Console.WriteLine(strrev);
+            Console.ReadKey();
+        }
+    }
+}
