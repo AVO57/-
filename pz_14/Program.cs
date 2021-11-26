2c04281da9fbe675636584edfb5891342af1e119
AVO57 <grigoryan.edmond16@mail.ru>
2021-11-26 17:54:24 +0500

    test edit

---
+++ pz_14/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+
+namespace Sobes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string path = "C:\\pz\\output.txt";
+            StreamWriter sw = new StreamWriter(path);
+            string read = Console.ReadLine();
+            while (read != "")
+            {
+                sw.WriteLine(read); // Записываем в файл строку
+                                    // Если файл отстутвовал, то создаётся автоматически
+                read = Console.ReadLine();
+            }
+            sw.Close();
+            Console.WriteLine("Операция завершена!"); // Операция завершена
+
+        }
+    }
+}
