c9c6217935ee2eb38630a6b0a2bffcbde655a63b
AVO57 <grigoryan.edmond16@mail.ru>
2021-11-26 18:08:49 +0500

    test edit

--- pz_14/Program.cs
+++ pz_14/Program.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.IO;
 
-namespace Sobes
+namespace pz_14
 {
     class Program
     {
---
+++ pz_15/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+
+namespace pz_15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = "C:\\pz\\output.txt";
+            string[] str = File.ReadAllLines(path);
+            Console.WriteLine("Количество строк: " + str.Length); // Выводим количество строк
+            for (int i = 0; i < str.Length; i++) // Перебираем каждую строку
+            {
+                int x = 0;
+                char[] ch = str[i].ToCharArray();
+                for (int d = 0; d < ch.Length; d++) // Проверяем каждый символ на знак препинания
+                {
+                    if (ch[d] == '.' || ch[d] == ',' || ch[d] == '!' || ch[d] == ':' || ch[d] == '?' || ch[d] == ';') x++;
+                }
+                Console.WriteLine("Количество знаков препинания в " + i + " строке: " + x);
+
+            }
+        }
+    }
+}
